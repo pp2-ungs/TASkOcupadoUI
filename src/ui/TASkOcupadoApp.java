6e4d231daf52963a0b7420657cc8f30e45dccb12
Lucifer! <6011234+rondelli@users.noreply.github.com>
2024-09-18 09:34:13 -0300

    Initial commit

---
+++ src/ui/TASkOcupadoApp.java
@@ -0,0 +1,25 @@
+package ui;
+
+import core.Notificator;
+import core.TASkOcupado;
+import core.ConcreteNotificator;
+
+public class TASkOcupadoApp {
+
+    public static void main(String[] args) {
+        var taskOcupado = new TASkOcupado();
+        var taskOcupadoController = new TASkOcupadoController(taskOcupado);
+
+        Notificator notificator = new ConcreteNotificator();
+
+        taskOcupado.getTaskAssigner().addObserver(notificator);
+
+        // taskOcupadoView display
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new TASkOcupadoView(taskOcupado, taskOcupadoController).setVisible(true);
+            }
+        });
+    }
+
+}
---
+++ src/ui/TASkOcupadoController.java
@@ -0,0 +1,25 @@
+package ui;
+
+import core.Member;
+import core.TASkOcupado;
+import core.Observable;
+import core.Observer;
+import core.Task;
+
+public class TASkOcupadoController implements Observer {
+
+    private TASkOcupado taskOcupado;
+
+    public TASkOcupadoController(TASkOcupado taskOcupado) {
+        this.taskOcupado = taskOcupado;
+        this.taskOcupado.addObserverToAssigner(this);
+    }
+
+    public void assignTask(String taskDescription, String memberName) {
+        taskOcupado.assignTask(taskDescription, memberName);
+    }
+
+    @Override
+    public void update(Object obj) {
+    }
+}
