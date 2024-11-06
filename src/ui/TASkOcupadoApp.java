64214d4be9bb93003c774b6658ea25117abcada6
Gonza López <gonzagonzalopez20@gmail.com>
2024-11-06 14:39:27 -0300

    Add config path in main

--- src/ui/TASkOcupadoApp.java
+++ src/ui/TASkOcupadoApp.java
@@ -3,10 +3,13 @@ package ui;
 import core.CoreFactory;
 
 public class TASkOcupadoApp {
-
+    
+     /**
+     * @param args[0] = config.properties path
+     */
     public static void main(String[] args) {
         // init model
-        var taskOcupado = new CoreFactory("").create();
+        var taskOcupado = new CoreFactory(args.length > 0 ? args[0]: "").create();
 
         // init view
         java.awt.EventQueue.invokeLater(new Runnable() {
