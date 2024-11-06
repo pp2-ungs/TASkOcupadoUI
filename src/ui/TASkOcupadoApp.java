1b25499639c5909eda79ef8a22ad7c7935c4686d
Gonza López <gonzagonzalopez20@gmail.com>
2024-11-06 20:25:05 -0300

    Add Logger

--- src/ui/TASkOcupadoApp.java
+++ src/ui/TASkOcupadoApp.java
@@ -1,22 +1,26 @@
 package ui;
 
 import core.CoreFactory;
+import java.io.File;
+import logger.Logger;
 
 public class TASkOcupadoApp {
 
     /**
-     * @param args[0] = path de config.properties
+     * @param args[0] = config.properties path
      */
     public static void main(String[] args) {
+        
+        // init logger
+        var fileName = System.getProperty("user.home") + File.separator + "AssignmentLogger.txt"; // FIXME
+        var logger = new Logger(fileName);
+        
         // init model
         var taskOcupado = new CoreFactory(args.length > 0 ? args[0]: "").create();
+        taskOcupado.addObserver(logger);
 
         // init view
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new TASkOcupadoView(taskOcupado).setVisible(true);
-            }
-        });
+        java.awt.EventQueue.invokeLater(() -> new TASkOcupadoView(taskOcupado));
     }
 
 }
--- src/ui/TASkOcupadoView.java
+++ src/ui/TASkOcupadoView.java
@@ -23,6 +23,7 @@ public class TASkOcupadoView extends javax.swing.JFrame implements observer.Obse
 
     public TASkOcupadoView(TASkOcupado taskOcupado) {
         initComponents();
+        setVisible(true);
         setAppearance(DARK);
         setLocationRelativeTo(null);
 
